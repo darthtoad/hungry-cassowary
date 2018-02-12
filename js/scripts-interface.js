30dca0a55ef0fea3d3b02f529f4d5adedb22c6e3
Samuel Gespass and Zach Evans <samuel@>
2018-02-12 11:41:24 -0800

    add difficulty levels

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,23 +1,47 @@
 import { HungryCassowary } from "./../js/scripts.js";
 
 $(document).ready(function(){
-  let kerchov = new HungryCassowary("Kerchov");
-  kerchov.setHunger();
-  let thingy = setInterval(() => {
-    if (kerchov.didYouGetKickedInTheBalls()) {
-      alert("You just got kicked in the balls because you didn't feed Kerchov!");
-      $("#pic").empty();
-      $("#pic").append('<span title="You suck because you didn\'t feed it!"><img src="./../img/Cassowary-attack.jpg" alt="Angry Cassowary" /></span>');
-    }
-  }, 1000);
+  $("#form").submit(function(event){
+    event.preventDefault();
+    $("#pic").empty();
+    $("#pic").append('<span title="Feed kerchov!"><img src="/img/cassowary.jpg" alt="Cassowary" /></span>')
+    $("#form").hide();
+    $("#game").show();
+    const difficulty = $("input:radio[name=difficulty]:checked").val()
+    let kerchov = new HungryCassowary("Kerchov", difficulty);
+    kerchov.setHunger();
+    let thingy = setInterval(() => {
+      $("#foodLevel").empty();
+      if (kerchov.didYouGetKickedInTheBalls()) {
+        alert("You just got kicked in the balls because you didn't feed Kerchov!");
+        $("#pic").empty();
+        $("#pic").append('<span title="You suck because you didn\'t feed it!"><img src="./../img/Cassowary-attack.jpg" alt="Angry Cassowary" /></span>');
+        $("#form").show();
+      }
+      $("#foodLevel").append(`Kerchov's current food level is: ${kerchov.foodLevel}<br>
+      His sleep level is: ${kerchov.sleepLevel}<br>His mood level is: ${kerchov.moodLevel}`
+    );
+    }, 500);
 
-  let clear = setInterval(() => {
-    if (kerchov.didYouGetKickedInTheBalls()) {
-      clearInterval(thingy);
-    }
-  }, 1050);
+    let clear = setInterval(() => {
+      if (kerchov.didYouGetKickedInTheBalls()) {
+        clearInterval(thingy);
+      }
+    }, 500);
 
-  $("#button").click(function(){
-    kerchov.feed();
+    $("#button").click(function(event){
+      event.preventDefault();
+      kerchov.feed();
+    })
+
+    $("#sleep").click(function(event){
+      event.preventDefault();
+      kerchov.putToBed();
+    })
+
+    $("#play").click(function(event){
+      event.preventDefault();
+      kerchov.play();
+    })
   })
 })
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,56 @@
 export class HungryCassowary {
-  constructor(name) {
+  constructor(name, difficulty) {
     this.name = name;
+    this.difficulty = difficulty;
     this.foodLevel = 10;
+    this.sleepLevel = 10;
+    this.moodLevel = 10;
   }
 
   setHunger() {
-    setInterval(() => {
-      this.foodLevel--;
-      console.log(this.foodLevel);
-    }, 1000);
+    if (this.difficulty === "easy") {
+      setInterval(() => {
+        this.foodLevel--;
+        this.sleepLevel--;
+        this.moodLevel--;
+        console.log(this.foodLevel);
+      }, 2000);
+    } else if (this.difficulty === "medium") {
+      setInterval(() => {
+        this.foodLevel--;
+        this.sleepLevel--;
+        this.moodLevel--;
+        console.log(this.foodLevel);
+      }, 1000);
+    } else {
+      setInterval(() => {
+        this.foodLevel--;
+        this.sleepLevel--;
+        this.moodLevel--;
+        console.log(this.foodLevel);
+      }, 500);
+    }
+
   }
 
   didYouGetKickedInTheBalls() {
-      if (this.foodLevel > 0) {
-        return false;
-      } else {
-        // console.log("Kicked in balls");
-        return true;
-      }
+    if (this.foodLevel > 0 && this.sleepLevel > 0 && this.moodLevel > 0) {
+      return false;
+    } else {
+      // console.log("Kicked in balls");
+      return true;
+    }
   }
 
   feed() {
     this.foodLevel = 10;
+  }
+
+  putToBed() {
+    this.sleepLevel = 10;
+  }
+
+  play() {
+    this.moodLevel = 10;
   }
 }
